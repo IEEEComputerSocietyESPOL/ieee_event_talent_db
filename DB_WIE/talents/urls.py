f651bf6077f86258fd948cffeaccdd9394fc5780
John0487 <jocarren@espol.edu.ec>
2026-06-09 14:01:54 -0500

    Refactor: Sincronizar estructura de carpetas limpia directamente en la raíz

--- DB_WIE/talents/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registro/', views.registro, name='registro'),
-    path('registro/exito/', views.registro_exito, name='registro_exito'), # <-- Asegúrate de que tenga el 'registro/' antes
-]
